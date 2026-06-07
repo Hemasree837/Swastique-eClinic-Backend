745c62ce35ccd53842b8c4866d5dc1f0c672faa1
Hemasree837 <hemasree837@gmail.com>
2026-06-07 18:07:56 +0530

    Added Vercel URL to CORS

--- src/main/java/com/clinic/config/SecurityConfig.java
+++ src/main/java/com/clinic/config/SecurityConfig.java
@@ -38,7 +38,10 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "https://swastique-e-clinic-frontend.vercel.app"
+        	));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
